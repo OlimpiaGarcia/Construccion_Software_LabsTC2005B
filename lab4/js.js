561a1996f387046e77fe820a7ef10ee635cd1859
olimpia garcia <olimpia.g24@gmail.com>
2023-02-17 13:10:29 -0600

    Se copia lab1 para agregar framework

--- lab4/js.js
+++ lab4/js.js
@@ -34,4 +34,67 @@ for (let j = 1; j <= 10; j++)
 console.log(j); //j no existe, se murio
 
 
+alert ("Hola")
+let nombre = prompt ("Como te llamas"); // permite agregar tenxto en una ventana emergente
+console.log(nombre);
+
+let is_hungry = confirm("tines hambre?");
+console.log(is_hungry);
+
+
+// funciones tradicionales
+function numero_alitas() {
+    return 12;
+}
+
+console.log(numero_alitas());
+
+//arrow function
+let cantidad_alitas = () => { return 12 };
+console.log(cantidad_alitas());
+
+let boton = document.getElementById("contador de clicks");
+boton.innerHTML =  "cuenta clicks";
+boton.onclick = () =>  //funcion anonima
+{
+    contaodr_clicks++;
+    console.log(contaodr_clicks);
+    document.getElementById("clicks").innerHTML = contaodr_clicks;
+}
+
+//arreglo
+const arreglo = ["Elemento"]; //es const por que siempre es arrglo, lo que esta dentro no es constante
+arreglo.push("otro elemento"); //el tipo de dato en los arreglos es dinamico, puedes meterle lo que quieras y campechanear
+console.log(arreglo);           // ya sea numero. o string o cosas asi
+arreglo ["dos"] = 2;
+arreglo.length = 10;
+console.log(arreglo);
+
+
+//recorrer los valores del arreglo
+for (let valor of arreglo)
+{
+    console.log(valor);
+} 
+
+//recorrer los indices del arreglo
+for (let elemento in arreglo)
+{
+    console.log(elemento);
+}
+
+//objetos
+let objeto = 
+{
+    atributo_1: "valor 1",
+    atributo_2: 2
+}
+
+objeto.atributo_3 = arreglo;
+console.log(objeto);
+console.log(objeto.atributo_1);
+
+for (let atributo in objeto) {
+    console.log(atributo);
+}
 
