504e418a5448ba9d5c4a30c6e1d1b9ac7616e6e6
olimpia garcia <olimpia.g24@gmail.com>
2023-02-28 13:15:49 -0600

    Se separ el ervidor del contenido

--- lab11/app.js
+++ lab11/app.js
@@ -80,81 +80,9 @@ app.use('/home', (request, response, next) => {
     response.send('bienvenido a casa'); 
 });
 
-//app.use("/pedir", (request, response, next) =>  esto funciona para post y get
-app.get("/pedir", (request, response, next) => 
-{
+const hotcakesRutas = require("./routes/hotcakes.routes");
 
-    console.log (request.body);
-    console.log (request.body.cafe);
-
-    let html = `
-
-        <!DOCTYPE html>
-
-            <html>
-
-                <head>
-
-                <meta charset="utf-8">
-
-            </head>
-
-            <body>
-
-                <h1>Hot cakes</h1>
-
-                    <form action="/pedir" method="POST">
-
-                    <fieldset>
-
-                        <legend>Escoge tu desayuno:</legend>
-
-                            <div>
-
-                                <input type="checkbox" id="malteada" name="malteada">
-
-                                <label for="malteada">Malteada</label>
-
-                            </div>
-
-                            <div>
-                                <input type="checkbox" id="cafe" name="cafe">
-
-                                <label for="cafe">Café</label>
-
-                            </div>
-
-                            <div>
-
-                                <input type="number" id="hot_cakes" name="hot_cakes" value="0" min="0">
-
-                                <label for="hot_cakes"> hot cakes</label>
-
-                            </div>
-
-                        </fieldset>
-
-                        <br>
-
-                        <input type="submit" value="pedir">
-
-                    </form>
-
-                </body>
-
-            </html>
-
-        `;
-        response.send(html);
-
-});
-
-app.post("/pedir", (request, response, next) => 
-{
-    console.log(request.body);
-
-    response.send("Pediste " + request.body.hot_cakes + "hot cakes")
-})
+app.use("/lab11", hotcakesRutas);
 
 
 app.use((request, response, next) => {
---
+++ lab11/routes/hotcakes.routes.js
@@ -0,0 +1,84 @@
+// en este archivo esta la logica de la pagina 
+
+const express = require('express');
+
+const router = express.Router();
+
+//app.use("/pedir", (request, response, next) =>  esto funciona para post y get
+router.get("/pedir", (request, response, next) => 
+{
+
+    console.log (request.body);
+    console.log (request.body.cafe);
+
+    let html = `
+
+        <!DOCTYPE html>
+
+            <html>
+
+                <head>
+
+                <meta charset="utf-8">
+
+            </head>
+
+            <body>
+
+                <h1>Hot cakes</h1>
+
+                    <form action="/lab11/pedir" method="POST">
+
+                    <fieldset>
+
+                        <legend>Escoge tu desayuno:</legend>
+
+                            <div>
+
+                                <input type="checkbox" id="malteada" name="malteada">
+
+                                <label for="malteada">Malteada</label>
+
+                            </div>
+
+                            <div>
+                                <input type="checkbox" id="cafe" name="cafe">
+
+                                <label for="cafe">Café</label>
+
+                            </div>
+
+                            <div>
+
+                                <input type="number" id="hot_cakes" name="hot_cakes" value="0" min="0">
+
+                                <label for="hot_cakes"> hot cakes</label>
+
+                            </div>
+
+                        </fieldset>
+
+                        <br>
+
+                        <input type="submit" value="pedir">
+
+                    </form>
+
+                </body>
+
+            </html>
+
+        `;
+        response.send(html);
+
+});
+
+router.post("/pedir", (request, response, next) => 
+{
+    console.log(request.body);
+
+    response.send("Pediste " + request.body.hot_cakes + "  hot cakes")
+})
+
+
+module.exports = router; //exportar la ruta
