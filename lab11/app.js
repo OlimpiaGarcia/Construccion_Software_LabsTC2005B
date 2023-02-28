c2ad869a93d79fba9e55d71b81f5fddcf33c8d64
olimpia garcia <olimpia.g24@gmail.com>
2023-02-28 12:38:50 -0600

    se crea servidor

--- lab11/app.js
+++ lab11/app.js
@@ -7,3 +7,34 @@
 
 console.log("Hola desde nodemon")
 console.log("Hola desde npm")
+//const { request, response } = require('express');
+//--------------------------------------------------------
+
+
+const express = require('express');
+const app = express();   //esto crea el servidor
+
+/*middleware paso a paso
+
+app.use((request,response,next) => {})
+
+*/
+
+//Middleware: capas enmedio del software
+app.use((request, response, next) => {
+    console.log('Middleware!');
+    //Le permite a la petición avanzar hacia el siguiente middleware
+    next();  //solo puede haber un next con send
+});
+
+app.use((request, response, next) => {
+    console.log('Otro middleware!');
+    //Manda la respuesta
+    response.send('¡Hola mundo!'); 
+  
+});
+
+
+
+//esto escucha al servidor
+app.listen(3000);
