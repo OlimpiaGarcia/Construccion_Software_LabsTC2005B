83c4bf70c503971028e2f7126f15960dc8736f6a
olimpia garcia <olimpia.g24@gmail.com>
2023-02-28 12:17:40 -0600

    se agrega nodemon

--- lab11/app.js
+++ lab11/app.js
@@ -1,3 +1,5 @@
 //framework de desarrollo web: incluye librerias y guias de como trabajar
 //npm: instala paquetes, permite instalar dependencias cpn un solo comando
-//npm init= cuestionario lab11
+//npm init= cuestionario lab11 crear pacckage.json
+//nodemon = npm install --save-dev nodemon
+//no enviar node
