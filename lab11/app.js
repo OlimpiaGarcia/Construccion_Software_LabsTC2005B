a87a8619b28882e00c5fb70b0178708be3b6064e
olimpia garcia <olimpia.g24@gmail.com>
2023-02-28 12:24:39 -0600

    se instala express

--- lab11/app.js
+++ lab11/app.js
@@ -2,4 +2,8 @@
 //npm: instala paquetes, permite instalar dependencias cpn un solo comando
 //npm init= cuestionario lab11 crear pacckage.json
 //nodemon = npm install --save-dev nodemon
-//no enviar node
+//no enviar node con git ignore
+//npm install express = instalar framework express
+
+console.log("Hola desde nodemon")
+console.log("Hola desde npm")
