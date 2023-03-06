34eb6445c0475bee315d69eaa836c901540ef827
olimpia garcia <olimpia.g24@gmail.com>
2023-03-05 18:55:29 -0600

    se añade txt con respuestas al lab

--- lab11/app.js
+++ lab11/app.js
@@ -1,3 +1,6 @@
+//authors: Olimpia Garcia & Yuna Chung
+//Matricula: A01708462 & A01709043
+
 //framework de desarrollo web: incluye librerias y guias de como trabajar
 //npm: instala paquetes, permite instalar dependencias cpn un solo comando
 //npm init= cuestionario lab11 crear pacckage.json
